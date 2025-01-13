a0fc2f81c7920558e421f019b011846cc694e28a
Poggii <s8901744h@majorana-giorgi.edu.it>
2025-01-13 09:50:11 +0100

    update

--- src/main/java/Calcolatrice.java
+++ src/main/java/Calcolatrice.java
@@ -4,7 +4,46 @@ public class Calcolatrice {
 	{
 		double s = 0;
 		for(double addendo : addendi)
-			s = s+addendo;
+		{
+			if(addendo < 0) {
+				System.out.println("addendo è negativo");
+			}
+			if(addendo == 0) {
+				System.out.println("addendo è 0");
+			}
+			if(addendo > 0) {
+				System.out.println("addendo è positivo");
+			}
+			s = s+ addendo;
+		}
 		return s;
+	}
+	
+	public double differenza (double d1, double d2) {
+		double diff = 0;
+		diff = d1-d2;
+		return diff;
+	}
+	
+	
+	public double moltiplicazione(double...moltiplicandi)
+	{
+		double s=1;
+		for(double moltiplicando:moltiplicandi)
+		{
+			s=s*moltiplicando;
+		}
+		return s;
+	}
+	
+	public double divisione(double divisore, double dividendo) throws Exception
+	{
+		double ris;
+		ris = divisore/dividendo;
+		if(dividendo==0)
+		{
+			throw new Exception();
+		}
+		return ris;
 	}
 }
--- src/test/java/TestCalcolatrice.java
+++ src/test/java/TestCalcolatrice.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Assert;
 
 import org.junit.Test;
@@ -6,21 +9,138 @@ public class TestCalcolatrice {
 
 	@Test
 	public void testSomma() 
-	{
+	{	
 		Calcolatrice c = new Calcolatrice();
 		
-		double res = c.somma(5,6);
+		double res =  c.somma(5,6);
 		
-		Assert.assertEquals(11, res, 0);
+		assertEquals(11, res);
 	}
-
 	@Test
 	public void testSomma2() 
 	{
 		Calcolatrice c = new Calcolatrice();
 		
-		double res = c.somma(6,6);
+		double res =  c.somma(6,6);
 		
-		Assert.assertEquals(12, res, 0);
+		assertEquals(12, res);
+	}
+	@Test
+	public void testSommaNegativo() 
+	{
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.somma(10,-5);
+		
+		assertEquals(5, res);
+	}
+	@Test
+	public void testSott() 
+	{	
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.differenza(5,6);
+		
+		assertEquals(-1, res);
+	}
+	@Test
+	public void testSott2() 
+	{
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.differenza(6,6);
+		
+		assertEquals(0, res);
+	}
+	@Test
+	public void testSottNeg() 
+	{
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.differenza(-5,-3);
+		
+		assertEquals(-2, res);
+	}
+	@Test
+	public void testMolt() 
+	{	
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.moltiplicazione(5,6);
+		
+		assertEquals(30, res);
+	}
+	@Test
+	public void testMolt2() 
+	{	
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.moltiplicazione(6,6);
+		
+		assertEquals(36, res);
+	}
+	@Test
+	public void testMoltNeg() 
+	{	
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.moltiplicazione(-5,6);
+		
+		assertEquals(-30, res);
+	}
+	@Test
+	public void testDiv()  throws Exception 
+	{	
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.divisione(8,8);
+		
+		assertEquals(1, res);
+	}
+	@Test
+	public void testDiv2()  throws Exception 
+	{	
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.divisione(20,4);
+		
+		assertEquals(5, res);
+	}
+	@Test
+	public void testDivNeg()  throws Exception 
+	{	
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.divisione(-5,10);
+		
+		assertEquals(-0.5, res);
+	}
+	
+	@Test
+	public void testDiv3()  throws Exception 
+	{	
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =  c.divisione(-5,10);
+		
+		assertNotEquals(82, res);
+	}
+	@Test
+	public void testDiv0() throws Exception 
+	{	
+		Calcolatrice c = new Calcolatrice();
+		
+		try
+		{
+			double res =  c.divisione(5,0);
+			assertEquals(null, res);
+		}
+		catch(Exception e)
+		{
+			System.out.println("impossibile div per 0");
+			
+		}
+		
+		
 	}
 }
